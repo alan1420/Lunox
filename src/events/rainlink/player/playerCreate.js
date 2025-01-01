2361e83da39e936c3a9e3a9195d2da82126471d5
Rahmadillah Maulana <rahmadillah.maulana11@gmail.com>
2025-01-01 08:39:22 +0700

    Update playerCreate.js

--- src/events/rainlink/player/playerCreate.js
+++ src/events/rainlink/player/playerCreate.js
@@ -1,12 +1,28 @@
+import fetch from 'node-fetch';
+
 module.exports = async (client, player) => {
     const guild = await client.guilds.cache.get(player.guildId);
     const guildData = client.data.get(`guildData_${guild.id}`);
+    const session = player.voice.sessionId
 
     if (guildData.reconnect.status) {
         console.debug(`[DEBUG] Player reconnected to [${guild.name}] (${guild.id})`);
     } else {
         console.debug(`[DEBUG] Player created in [${guild.name}] (${guild.id})`);
     }
+
+    let url = `http://localhost:2333/v4/sessions/${session}/players/${guild.id}?noReplace=false`;
+
+    let options = {
+      method: 'PATCH',
+      headers: {'Content-Type': 'application/json', Authorization: 'youshallnotpass'},
+      body: '{"filters":{"pluginFilters":{"normalization":{"maxAmplitude":0.9,"adaptive":true}}}}'
+    };
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.error('error:' + err));
 };
 
 /**

43b39a491bbd030b1be4c3899c66f4e086e6350e
Rahmadillah Maulana <rahmadillah.maulana11@gmail.com>
2025-01-01 17:09:31 +0700

    feat: bot leaves if it joined without queue

--- src/events/rainlink/player/playerCreate.js
+++ src/events/rainlink/player/playerCreate.js
@@ -1,7 +1,10 @@
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = async (client, player) => {
     const guild = await client.guilds.cache.get(player.guildId);
     const guildData = client.data.get(`guildData_${guild.id}`);
     const session = player.node.driver.sessionId
+    const channel = await client.channels.cache.get(player.textId);
 
     if (guildData.reconnect.status) {
         console.debug(`[DEBUG] Player reconnected to [${guild.name}] (${guild.id})`);
@@ -21,6 +24,21 @@ module.exports = async (client, player) => {
         .then(res => res.json())
         .then(json => console.log(JSON.stringify(json, null, 2)))
         .catch(err => console.error('error:' + err));
+    
+    const embed = new EmbedBuilder()
+        .setColor(client.config.embedColor)
+        .setDescription(`The queue is empty. You can disable this by using \`247\` command.`);
+
+    await delay(client.config.leaveTimeout);
+    
+    const guildData = client.data.get(`guildData_${guild.id}`);
+    if (guildData && !guildData.reconnect.status) {
+        const isNotPlaying = !player.playing && !player.queue.currennt;
+        if (isNotPlaying) {
+            await player.destroy();    
+            if (channel) await channel.send({ embeds: [embed] });
+        }
+    }
 };
 
 /**
